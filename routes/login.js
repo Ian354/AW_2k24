62f3bc99585d5cc71de92506643a732031f17804
Ian <ianalvarezdf@gmail.com>
2024-11-14 17:31:51 +0100

    contraseñas y eso

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+const pool= mysql.createPool({
+    host: "localhost",
+    user: "root",
+    host: "",
+    database: "AW_24"
+});
 
 router.get('/', (req, res) => {
     res.render('login'); //acceder al formulario de log in
+})
+
+router.post('/insert', (req, res) => {
+    const {correo, contraseña} = req.body;
+    const query = "SELECT * FROM Usuarios WHERE correo = ?";
+
+    pool.query(query, [correo], (err, results) => {
+        if (err) throw err;
+
+        if(results.length == 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const user = results[0];
+
+        if (contraseña !== user.contraseña) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        console.log(`usuario ${user.nombre} logeado correctamente`);
+        res.redirect('/');
+    })
 })
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+const pool= mysql.createPool({
+    host: "localhost",
+    user: "root",
+    host: "",
+    database: "AW_24"
+});
 
 router.get('/', (req, res) => {
     res.render('registro'); //acceder al formulario de registro
 })
 
-app.post("/insert", (req, res) => {
+router.post("/insert", (req, res) => {
     const {nombre, telefono, facultad, correo, contraseña} = req.body;
     const query= `INSERT INTO Usuarios (nombre, telefono, facultad, correo, contraseña, lista_negra) VALUES (?, ?, ?, ?, ?, 0)`;
     pool.query(query, [nombre, telefono, facultad, correo, contraseña], (err) => {
