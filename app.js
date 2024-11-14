740bdbd6fcf5331bf35537a9c84ccce967552877
acatocaton <73439737+acatocaton@users.noreply.github.com>
2024-11-14 16:53:14 +0100

    conexion BD

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql');
 const port = 3000;
 const app = express(); //app iniciada con express
 
@@ -6,6 +7,13 @@ app.set('view engine', 'ejs'); //Las vistas se manejan con ejs
 app.use(express.urlencoded({ extended : true }))
 
 app.use(express.static('public'));
+
+const pool= mysql.createPool({
+    host: "localhost",
+    user: "root",
+    host: "",
+    database: "AW_24"
+});
 
 const loginRouter = require('./routes/login'); //router para los logins
 const registerRouter = require('./routes/register'); //router para los registros
--- routes/register.js
+++ routes/register.js
@@ -5,4 +5,15 @@ router.get('/', (req, res) => {
     res.render('registro'); //acceder al formulario de registro
 })
 
+app.post("/insert", (req, res) => {
+    const {nombre, telefono, facultad, correo, contraseña} = req.body;
+    const query= `INSERT INTO Usuarios (nombre, telefono, facultad, correo, contraseña, lista_negra) VALUES (?, ?, ?, ?, ?, 0)`;
+    pool.query(query, [nombre, telefono, facultad, correo, contraseña], (err) => {
+        if (err) throw err;
+
+        console.log(` ${nombre}, se ha registrado correctamente.`);
+        return res.redirect("/");
+    });
+});
+
 module.exports = router;
