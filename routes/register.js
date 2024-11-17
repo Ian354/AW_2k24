c06ecaa3c014913ff9a33432568d8fd3fb2fdd2e
Ianal <ianalvarezdf@gmail.com>
2024-11-17 12:19:47 +0100

    check de formulario registro

--- routes/register.js
+++ routes/register.js
@@ -13,8 +13,10 @@ router.get('/', (req, res) => {
     res.render('registro'); //acceder al formulario de registro
 })
 
-router.post("/insert", (req, res) => {
+router.post("/", validacionCorreo, validacionTelefono, (req, res) => {
     const {nombre, telefono, facultad, correo, contraseña} = req.body;
+    if(!telefono) telefono = 0; //si no hay telefono definido se pone a 0
+
     const query= `INSERT INTO Usuarios (nombre, telefono, facultad, correo, contraseña, lista_negra) VALUES (?, ?, ?, ?, ?, 0)`;
     pool.query(query, [nombre, telefono, facultad, correo, contraseña], (err) => {
         if (err) throw err;
@@ -23,5 +25,38 @@ router.post("/insert", (req, res) => {
         return res.redirect("/");
     });
 });
+
+function validacionTelefono(req, res, next) {
+    let { telefono } = req.body;
+
+    if(!telefono) //si no se ha definido telefono se salta
+        next();
+
+    telefono = telefono.replace(/\s+/g, ""); //elimina todos los espacios del numero de telefono
+
+    const nueveDig = /^\d{9}$/.test(telefono);
+    const doceDig = /^\+34\d{9}$/.test(telefono);
+
+    if (!nueveDig && !doceDig) {
+        return res.render("registro", { 
+            error: "El teléfono debe tener 9 dígitos o 12 si comienza con +34.", 
+            formData: req.body 
+        });
+    }
+    next();
+}
+
+function validacionCorreo(req, res, next) {
+    const { correo } = req.body;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@ucm\.es$/;
+
+    if (!correo || !emailRegex.test(correo)) {
+        return res.render("registro", { 
+            error: "El correo debe terminar con @ucm.es.", 
+            formData: req.body 
+        });
+    }
+    next();
+}
 
 module.exports = router;
