b228524a37de1afebeee85bedbb8e521c184473d
acatocaton <73439737+acatocaton@users.noreply.github.com>
2024-11-20 15:55:48 +0100

    usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql');
+
+const pool= mysql.createPool({
+    host: "localhost",
+    user: "root",
+    host: "",
+    database: "AW_24"
+});
 
 const eventosInscritosRouter = require('./eventosInscritos'); //router para eventosInscritos
 const misEventosRouter = require('./misEventos'); // router para misEventos
@@ -10,5 +18,23 @@ router.use('/eventos', misEventosRouter);
 router.get('/', (req, res) => {
     res.render('perfilUsuario');
 })
+
+// Ruta para mostrar el perfil del usuario
+router.get('/', (req, res) => {
+    // const userId = req. 
+    const query = `SELECT * FROM Eventos 
+    WHERE fecha > CURDATE()
+    OR (fecha = CURDATE() AND hora > CURTIME())`;   
+    pool.query(query,  (err, results) => {
+        if (err) {
+            throw err;
+        }
+
+        // Renderizar la vista de perfil pasando los datos del usuario
+        res.render('perfilUsuario', {
+            user:results
+        });
+    });
+});
 
 module.exports = router;
