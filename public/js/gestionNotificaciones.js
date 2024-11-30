09fced6484335a414ad6b8ef9875fe01de8f3d11
Ian <ianalvarezdf@gmail.com>
2024-11-30 14:16:44 +0100

    avance ajax

--- public/js/gestionNotificaciones.js
+++ public/js/gestionNotificaciones.js
@@ -1,22 +1,93 @@
 $(document).ready(function() {
-    // Handle previous email button click
+    // Click a la notificación
+    $('.notif-item').on('click', function() {
+        var emailId = $(this).data('email-id');
+        $('.email-content').hide();
+        $('#contenido-' + emailId).show();
+    });
+
+    // Notificacion previa
     $('.prev-notif').on('click', function() {
         var emailId = $(this).data('email-id');
-        var prevEmailId = emailId - 1;
+        var prevEmailId = emailId + 1;
         if (prevEmailId >= 0) {
             $('.notif-content').hide();
             $('#contenido-' + prevEmailId).show();
-        }
+        }esults, mostrados.length
     });
 
-    // Handle next email button click
+    // Notifiacion siguiente
     $('.next-notif').on('click', function() {
         var emailId = $(this).data('email-id');
-        var nextEmailId = emailId + 1;
+        var nextEmailId = emailId - 1;
+        console.log($('.list-group').length)
         if ($('#contenido-' + nextEmailId).length) {
             $('.notif-content').hide();
             $('#contenido-' + nextEmailId).show();
         }
     });
-    
 });
+
+setInterval(function() {
+    //get the value of the variable notificaciones declared in the inbox.ejs file
+    $.ajax({
+        url: '/inbox/fetch-notifications',
+        method: 'GET',
+        success: function(response) {
+            var notifications = response.notifications;
+            var notificationCount = response.notificationCount;
+            console.log(notificationCount);
+            var emailListHtml = '';
+            var emailContentHtml = '';
+            for(var i = notificationCount + notifications.length - 1; i >= notificationCount; i--) {
+                var notificacion = notifications[i - notificationCount];
+                if (!$('#titulo-' + i).length) {
+                    emailListHtml += `
+                        <li class="list-group-item shadow-sm rounded notif-item" id="titulo-${i}" data-email-id="${i}">
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <div class="initial-avatar bg-success bg-gradient">EU</div>
+                                </div>
+                                <div class="col-sm-8">
+                                    <div>
+                                        <p class="mb-1 text-singleline text-dark">Eventos UCM Team </p>
+                                        <p class="text-muted mb-0 text-singleline">${notificacion.titulo}</p>
+                                    </div>
+                                </div>
+                                <div class="col-sm-2">
+                                    <div>
+                                        <p class="mb-1 text-muted">${new Date(notificacion.hora).toLocaleTimeString('es-ES', { hour: 'numeric', minute: 'numeric'})}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    `;
+                    emailContentHtml += `
+                        <div class="notif-content" id="contenido-${i}" style="display: none;">
+                            <div class="p-2 pt-4">
+                                <div class="btn-toolbar mb-3 d-flex justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+                                    <div class="input-group">
+                                        <button type="button" class="btn text-muted prev-notif" data-email-id="${i}"><i class="fa-solid fa-chevron-left"></i></button>
+                                        <button type="button" class="btn text-muted next-notif" data-email-id="${i}"><i class="fa-solid fa-angle-right"></i></button>
+                                    </div>
+                                    <div class="btn-group me-2" role="group" aria-label="First group">
+                                        <button type="button" class="btn text-muted"><i class="fa-solid fa-trash-can"></i></button>
+                                    </div>
+                                </div>
+
+                                <p class="text-muted mb-1">Fecha: ${new Date(notificacion.hora).getDate()}/${new Date(notificacion.hora).getMonth()} ${new Date(notificacion.hora).toLocaleTimeString('es-ES', { hour: 'numeric', minute: 'numeric'})}</p>
+                                <h5 class="mb-3">${notificacion.titulo}</h5>
+
+                                <div style="font-family: Arial, sans-serif;">
+                                    <p class="mb-3">${notificacion.contenido}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+                }
+            }
+            $(`#titulo-${notificationCount - 1}`).prepend(emailListHtml);
+            $(`#contenido-${notificationCount - 1}`).prepend(emailContentHtml);
+        }
+    });
+}, 1000);
--- routes/inbox.js
+++ routes/inbox.js
@@ -10,14 +10,41 @@ const pool= mysql.createPool({
 });
 
 router.get('/', (req, res) => {
-    const query = 'SELECT * FROM notificaciones WHERE id_usuario = ?';
+    const query = 'SELECT * FROM notificaciones WHERE id_usuario = ? ORDER BY id ASC';
     pool.query(query, [req.session.userId], (err, results) => {
         if(err) throw err;
 
-        res.render('inbox', {
-            notificaciones: results
+        //set mostrado to 1 for all notifications
+        const updateQuery = 'UPDATE notificaciones SET mostrado = 1 WHERE id_usuario = ?';
+        pool.query(updateQuery, [req.session.userId], (err) => {
+            if(err) throw err;
+
+            res.render('inbox', {
+                notificaciones: results
+            });
         });
     })
 })
+
+router.get('/fetch-notifications', (req, res) => {
+    // Obtener las notificaciones que no se han mostrado
+    const query = 'SELECT * FROM notificaciones WHERE id_usuario = ? AND mostrado = 0 ORDER BY id ASC';
+    pool.query(query, [req.session.userId], (err, results) => {
+        if(err) throw err;
+
+        pool.query('SELECT id FROM notificaciones WHERE id_usuario = ? AND mostrado = 1', [req.session.userId], (err, mostrados) => {
+            // Actualizar mostrado a 1 para todas las notificaciones
+            pool.query('UPDATE notificaciones SET mostrado = 1 WHERE id_usuario = ?', [req.session.userId], (err) => {
+                if(err) throw err;
+
+                res.json({
+                    notifications: results,
+                    notificationCount: mostrados.length
+                });
+            });
+        });
+    }); 
+})
+
 
 module.exports = router;
